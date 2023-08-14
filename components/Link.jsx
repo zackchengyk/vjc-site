9108a0cd5e80e60386b6bcec78848747821bbec1
Zack Cheng <zack_cheng@brown.edu>
2023-08-14 21:54:32 +0800

    Attempt to fix link

--- components/Link.jsx
+++ components/Link.jsx
@@ -4,11 +4,19 @@ import { usePageContext } from '../renderer/usePageContext'
 export { Link }
 
 Link.propTypes = {
-  className: PropTypes.string,
   href: PropTypes.string.isRequired,
+  className: PropTypes.string,
 }
+
 function Link(props) {
-  const pageContext = usePageContext()
-  const className = [props.className, pageContext.urlPathname === props.href && 'is-active'].filter(Boolean).join(' ')
+  if (!props.href.startsWith('/')) throw new Error('Link href should start with /')
+  const cleanedHREF = normalize(import.meta.env.BASE_URL + props.href)
+  const urlPathname = usePageContext().urlPathname
+  console.log(urlPathname)
+  const className = [props.className, urlPathname === cleanedHREF && 'is-active'].filter(Boolean).join(' ')
   return <a {...props} className={className} />
+}
+
+function normalize(url) {
+  return '/' + url.split('/').filter(Boolean).join('/')
 }
