5ab3cc8d2c3559e0bcdcade75d45cfe1859dea7d
zackchengyk <zackchengyk@users.noreply.github.com>
2023-08-14 05:22:11 +0000

    deploy: 639b8f44166cfa9bab8bce15802ab0999f4877b2

---
+++ server/entries/pages_about_index-page.mjs
@@ -0,0 +1,16 @@
+import { a as jsxs, F as Fragment, j as jsx } from "../chunks/chunk-ac9dafa4.js";
+import "react/jsx-runtime";
+const code = "";
+function Page() {
+  return /* @__PURE__ */ jsxs(Fragment, { children: [
+    /* @__PURE__ */ jsx("h1", { children: "About" }),
+    /* @__PURE__ */ jsxs("p", { children: [
+      "Example of using ",
+      /* @__PURE__ */ jsx("code", { children: "vite-plugin-ssr" }),
+      "."
+    ] })
+  ] });
+}
+export {
+  Page
+};
