5b0d17d9c5f6a87370718570bc84c2e0d7413970
zackchengyk <zackchengyk@users.noreply.github.com>
2023-09-09 10:05:37 +0000

    deploy: 50d72cd2fb764a2987a228e942b137a1b919aeb8

---
+++ assets/entries/pages_team.page.1feaa41e.js
@@ -0,0 +1,1 @@
+import{j as e,F as r,a as n}from"../chunks/chunk-3abbb91c.js";const i={title:"Team"};function t(){return e(r,{children:[n("h1",{children:"Team"}),e("p",{children:["Example of using ",n("code",{children:"vite-plugin-ssr"}),"."]})]})}export{t as Page,i as documentProps};
