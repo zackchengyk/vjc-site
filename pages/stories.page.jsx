05bcd0ddbec173fbd3cfba88269ec9e0a728e18d
Zack Cheng <zack_cheng@brown.edu>
2023-09-12 12:59:44 +0800

    Implemented StoryWrapper

--- pages/stories.page.jsx
+++ pages/stories.page.jsx
@@ -1,3 +1,5 @@
+import { StoryWrapper } from '../resources/components/StoryWrapper'
+
 export { Page }
 export { documentProps }
 
@@ -6,8 +8,48 @@ const documentProps = { title: 'Stories' }
 function Page() {
   return (
     <>
-      <h1>Stories</h1>
-      <p>Todo</p>
+      <header>
+        <h1>Stories</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+      </header>
+      <section id="stories-section">
+        <StoryWrapper
+          facing="wall-left"
+          images={[
+            '/images/stories/test1/test1a.JPG',
+            '/images/stories/test1/test1b.JPG',
+            '/images/stories/test1/test1c.JPG',
+            '/images/stories/test1/test1d.JPG',
+          ]}
+        />
+        <StoryWrapper
+          facing="wall-right"
+          images={[
+            '/images/stories/test1/test1a.JPG',
+            '/images/stories/test1/test1b.JPG',
+            '/images/stories/test1/test1c.JPG',
+            '/images/stories/test1/test1d.JPG',
+          ]}
+        />
+        <StoryWrapper
+          facing="floor-left"
+          images={[
+            '/images/stories/test1/test1a.JPG',
+            '/images/stories/test1/test1b.JPG',
+            '/images/stories/test1/test1c.JPG',
+            '/images/stories/test1/test1d.JPG',
+          ]}
+        />
+        <StoryWrapper
+          facing="floor-right"
+          images={[
+            '/images/stories/test1/test1a.JPG',
+            '/images/stories/test1/test1b.JPG',
+            '/images/stories/test1/test1c.JPG',
+            '/images/stories/test1/test1d.JPG',
+          ]}
+        />
+      </section>
     </>
   )
 }
